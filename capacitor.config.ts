44357ed3490ab7cdeb183335c42007ae23aa1a33
marchewadm <merchwa.dawid@gmail.com>
2025-06-26 18:07:26 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DailyNews',
+  webDir: 'dist'
+};
+
+export default config;
